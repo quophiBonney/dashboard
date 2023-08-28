905cedc9c445680a08798450fdc29f7fd3c3620f
bonney <quophiclef@gmail.com>
2023-08-28 12:02:02 -0700

    more charts

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import {Typography} from "antd";
 import JobsChart from "./JobsChart";
 import BidsChart from "./BidsChart";
 import ProjectsChart from "./ProjectsChart";
 import AccountsChart from "./AccountsChart";
 import BarChart from "./BarChart";
-import DashboardWrapper from "./AccountsWrapper";
 import LineChart from "./LineChart";
-const Dashboard = ({dataSum, totalBids}) => {
+const Dashboard = () => {
   return (
     <div>
       <div className="container-fluid">
@@ -43,6 +41,19 @@ const Dashboard = ({dataSum, totalBids}) => {
             </div>
             <div className="card px-2 py-2">
               <LineChart />
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-9 col-lg-9 text-center mt-3">
+            <div className="card px-2 py-2">
+              <LineChart />
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-3 col-lg-3 text-center mt-3">
+            <div className="card px-2 py-2 mb-3">
+              <LineChart />
+            </div>
+            <div className="card px-2 py-2">
+              <BarChart />
             </div>
           </div>
         </div>
