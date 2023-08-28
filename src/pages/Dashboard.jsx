024e927b75fff472e3631c14c662b545315a666e
bonney <quophiclef@gmail.com>
2023-08-28 11:48:47 -0700

    addition of another line chart

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,6 +6,7 @@ import ProjectsChart from "./ProjectsChart";
 import AccountsChart from "./AccountsChart";
 import BarChart from "./BarChart";
 import DashboardWrapper from "./AccountsWrapper";
+import LineChart from "./LineChart";
 const Dashboard = ({dataSum, totalBids}) => {
   return (
     <div>
@@ -37,8 +38,11 @@ const Dashboard = ({dataSum, totalBids}) => {
             </div>
           </div>
           <div className="col-sm-12 col-md-3 col-lg-3 text-center mt-3">
-            <div className="card px-2 py-2">
+            <div className="card px-2 py-2 mb-3">
               <BarChart />
+            </div>
+            <div className="card px-2 py-2">
+              <LineChart />
             </div>
           </div>
         </div>
